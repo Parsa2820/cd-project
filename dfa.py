ef1b6eb96015d77ec280fb4e1889bd501d8e3aa2
Parsa Mohammadian <parsa2820@gmail.com>
2021-10-16 22:21:34 +0330

    Add DFA prototype

---
+++ dfa.py
@@ -0,0 +1,26 @@
+import re
+
+class DFA:
+    def __init__(self, states, transitions, initial, finals):
+        self.states = states
+        self.transitions = transitions
+        self.initial = initial
+        self.finals = finals
+
+    def run(self, word):
+        state = self.initial
+        for c in word:
+            available_transitions = [transition.next_state for transition in self.transitions if transition.match(state, c)]
+            if len(available_transitions) == 0:
+                return False
+            state = available_transitions[0].next_state
+        return state in self.finals
+
+class Transition:
+    def __init__(self, state, pattern, next_state):
+        self.state = state
+        self.pattern = pattern
+        self.next_state = next_state
+    
+    def match(self, state, c):
+        return re.match(self.pattern, c) and state == self.state
---
+++ test/dfa_test.py
@@ -0,0 +1,16 @@
+import unittest
+import dfa
+
+class DFATest(unittest.TestCase):
+    def test_run(self):
+        pass
+
+
+class TransitionTest(unittest.TestCase):
+    def test_match(self):
+        transition = dfa.Transition('0', '\d', '1')
+        self.assertTrue(transition.match('0', '4'))
+
+
+if __name__ == '__main__':
+    unittest.main()
