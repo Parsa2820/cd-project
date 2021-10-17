4ddcd77e5e0eec37f124b6d43a59e0e7630be698
Parsa Mohammadian <parsa2820@gmail.com>
2021-10-17 12:01:16 +0330

    Refactor scanner dfa

--- src/scanner/scanner.py
+++ src/scanner/scanner.py
@@ -4,18 +4,52 @@ from .dfa import DFA, Transition
 class Scanner:
     def __init__(self, program):
         self.program = program
-        transitions = [Transition(1, '[0-9]', 1), Transition(1, '[^0-9]|\x1A', 2), Transition(0, '[A-Za-z]', 3),
-                       Transition(3, '[A-Za-z0-9]', 3), Transition(3, '[^A-za-z0-9]|\x1A', 4),
-                       Transition(0, '[\n\t\f\v\r ]', 14),
-                       Transition(0, '[;:,\[\]\(\){}+\-*<]', 5), Transition(0, '=', 6),
-                       Transition(6, '=', 7), Transition(6, '[^=]', 8), Transition(0, '[0-9]', 1),
-                       Transition(0, '/', 9), Transition(9, '/', 10), Transition(9, '\*', 12),
-                       Transition(12, '[^*]', 12), Transition(12, '\*', 13), Transition(13, '\*', 13),
-                       Transition(13, '/', 11), Transition(13, '[^*]', 12), Transition(10, '\x1A|\n', 11),
-                       Transition(10, '[^\x1A\n]', 10)]
-        self.dfa_instance = DFA(states=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], transitions=transitions,
-                                initial=0,
-                                finals=[2, 4, 5, 8, 14, 11])
+        states = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        transitions = []
+        transitions.extend(self.get_number_transitions())
+        transitions.extend(self.get_id_keyword_transitions())
+        transitions.extend(self.get_symbol_transitions())
+        transitions.extend(self.get_comment_transitions())
+        transitions.extend(self.get_white_space_transitions())
+        initial = 0
+        finals = [2, 4, 5, 8, 14, 11]
+        self.dfa_instance = DFA(states, transitions, initial, finals)
+
+    def get_number_transitions(self):
+        number_transitions = [Transition(0, '[0-9]', 1),
+                              Transition(1, '[0-9]', 1),
+                              Transition(1, '[^0-9]|\x1A', 2)]
+        return number_transitions
+
+    def get_id_keyword_transitions(self):
+        id_keyword_transitions = [Transition(0, '[A-Za-z]', 3),
+                                  Transition(3, '[A-Za-z0-9]', 3),
+                                  Transition(3, '[^A-za-z0-9]|\x1A', 4)]
+        return id_keyword_transitions
+
+    def get_symbol_transitions(self):
+        symbol_transitions = [Transition(0, '[;:,\[\]\(\){}+\-*<]', 5),
+                              Transition(0, '=', 6),
+                              Transition(6, '=', 7),
+                              Transition(6, '[^=]', 8)]
+        return symbol_transitions
+
+    def get_comment_transitions(self):
+        comment_transitions = [Transition(0, '/', 9),
+                               Transition(9, '/', 10),
+                               Transition(9, '\*', 12),
+                               Transition(12, '[^*]', 12),
+                               Transition(12, '\*', 13),
+                               Transition(13, '\*', 13),
+                               Transition(13, '/', 11),
+                               Transition(13, '[^*]', 12),
+                               Transition(10, '\x1A|\n', 11),
+                               Transition(10, '[^\x1A\n]', 10)]
+        return comment_transitions
+
+    def get_white_space_transitions(self):
+        white_space_transitions = [Transition(0, '[\n\t\f\v\r ]', 14)]
+        return white_space_transitions
 
     def get_next_token(self):
         pointer = 0
