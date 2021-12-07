e67fd984b2b8f31d53fda8c952ce73c61a5186ca
Arian Yazdanparast <arian.yazdan2001@gmail.com>
2021-12-08 02:45:22 +0330

    first follow

--- src/parser/transitiondiagram.py
+++ src/parser/transitiondiagram.py
@@ -4,21 +4,49 @@ from share.token import Token, TokenType
 from share.symboltable import SymbolTable
 
 
+def is_in_token_set(input_token, token_set):
+    for token in token_set:
+        if token.type != input_token.type:
+            continue
+        elif token.type == TokenType.KEYWORD or token.type == TokenType.SYMBOL:
+            if token.value == input_token.value:
+                return True
+        else:
+            return True
+    return False
+
+
+def update_current_token():
+    TransitionDiagram.current_token = TransitionDiagram.scanner.next_token()
+
+
+class First:
+    def __init__(self, tokens, has_epsilon, has_dollar):
+        self.tokens = tokens
+        self.has_epsilon = has_epsilon
+
+
+
+class Follow:
+    def __init__(self, tokens, has_dollar):
+        self.tokens = tokens
+
+
+
 class TransitionDiagram:
     scanner: Scanner = None
     current_token: Token = None
 
-    def __init__(self, states, scanner, init_state):
+    def __init__(self, states, scanner, init_state, first, follow ):
         self.states = states
         self.init_state = init_state
+        self.first = first
+        self.follow = follow
 
     def parse(self):
         current_state = self.init_state
         while not current_state.is_final:
             pass
-
-    def update_current_token():
-        TransitionDiagram.current_token = TransitionDiagram.scanner.next_token()
 
 
 class State:
@@ -29,19 +57,31 @@ class State:
 
     def transmit(self, token):
         for transition in self.transitions:
-            if check_first_set():
-                return transition.next_state
+            if not transition.match_first(token):
+                continue
+            if transition.match(token):
+                TransitionDiagramtransition.destination_state
         return None
 
 
 class AbstractTransitionType:
+
+    def __init__(self, state):
+        self.destination_state = state
+
     def match(self, token):
+        pass
+
+    def match_first(self, token):
         pass
 
 
 class EpsilonTransition(AbstractTransitionType):
     def match(self, token):
         return True
+
+    def match_first(self, token):
+        return False
 
 
 class TerminalTransition(AbstractTransitionType):
@@ -54,18 +94,27 @@ class TerminalTransition(AbstractTransitionType):
         if token.type == TokenType.KEYWORD or token.type == TokenType.SYMBOL:
             if token.value != self.terminal.value:
                 return False
-        TransitionDiagram.update_current_token()
+        update_current_token()
         return True
 
-
+    def match_first(self, token):
+        return is_in_token_set(token, [self.terminal])
 
 
 class NonTerminalTransition(AbstractTransitionType):
-    def __init__(self, non_terminal_transtion_diagram: TransitionDiagram):
-        self.non_terminal_transtion_diagram = non_terminal_transtion_diagram
+    def __init__(self, non_terminal_transition_diagram: TransitionDiagram):
+        self.non_terminal_transition_diagram = non_terminal_transition_diagram
 
     def match(self, token):
         return token == self.non_terminal
+
+    def match_first(self, token):
+        if is_in_token_set(token, self.non_terminal_transition_diagram.first.tokens):
+            return True
+        if is_in_token_set(token, self.non_terminal_transition_diagram.follow.tokens) \
+                and self.non_terminal_transition_diagram.first.has_epsilon:
+            return True
+        return False
 
 
 class Transition:
