2b0bd30cc1a0c95f2b2b4c406dc814d0a0d79a57
Arian Yazdanparast <arian.yazdan2001@gmail.com>
2021-12-10 05:58:37 +0330

    define first 13 transitions

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,6 +1,33 @@
-from .auxiliaryset import Follow, First
-from .parsetree.parsetree import ParseTree, ParseTreeNode
-from .transitiondiagram import *
+from auxiliaryset import Follow, First
+from parsetree.parsetree import ParseTree, ParseTreeNode
+from transitiondiagram import *
+
+dollar_terminal = Token(TokenType.SYMBOL, '$')
+ID_terminal = Token(TokenType.ID, '')
+semi_column_terminal = Token(TokenType.SYMBOL, ';')
+bracket_open_terminal = Token(TokenType.SYMBOL, '[')
+bracket_close_terminal = Token(TokenType.SYMBOL, ']')
+pr_open_terminal = Token(TokenType.SYMBOL, '(')
+pr_close_terminal = Token(TokenType.SYMBOL, ')')
+NUM_terminal = Token(TokenType.NUM, '')
+int_terminal = Token(TokenType.KEYWORD, 'int')
+void_terminal = Token(TokenType.KEYWORD, 'void')
+comma_terminal = Token(TokenType.SYMBOL, ',')
+cr_open_terminal = Token(TokenType.SYMBOL, '{')
+cr_close_terminal = Token(TokenType.SYMBOL, '}')
+break_terminal = Token(TokenType.KEYWORD, 'break')
+if_terminal = Token(TokenType.KEYWORD, 'if')
+end_if_terminal = Token(TokenType.KEYWORD, 'endif')
+else_terminal = Token(TokenType.KEYWORD, 'else')
+repeat_terminal = Token(TokenType.KEYWORD, 'repeat')
+until_terminal = Token(TokenType.KEYWORD, 'until')
+return_terminal = Token(TokenType.KEYWORD, 'return')
+assign_terminal = Token(TokenType.SYMBOL, '=')
+less_terminal = Token(TokenType.SYMBOL, '<')
+equal_terminal = Token(TokenType.SYMBOL, '==')
+plus_terminal = Token(TokenType.SYMBOL, '+')
+minus_terminal = Token(TokenType.SYMBOL, '-')
+mul_terminal = Token(TokenType.SYMBOL, '*')
 
 
 class ParserBase:
@@ -19,33 +46,7 @@ class ParserBase:
 
 
 class CMinusParser(ParserBase):
-    dollar_terminal = Token(TokenType.SYMBOL , '$')
-    ID_terminal = Token(TokenType.ID , '')
-    semi_column_terminal = Token(TokenType.SYMBOL , ';')
-    cr_open_terminal = Token(TokenType.SYMBOL , '[')
-    cr_close_terminal = Token(TokenType.SYMBOL, ']')
-    pr_open_terminal = Token(TokenType.SYMBOL , '(')
-    pr_close_terminal = Token(TokenType.SYMBOL , ')')
-    NUM_terminal = Token(TokenType.NUM ,'')
-    int_terminal = Token(TokenType.KEYWORD , 'int')
-    void_terminal = Token(TokenType.KEYWORD, 'void')
-    comma_terminal = Token(TokenType.SYMBOL , ',')
-    bracket_open_terminal = Token(TokenType.SYMBOL , '{')
-    bracket_close_terminal = Token(TokenType.SYMBOL , '}')
-    break_terminal = Token(TokenType.KEYWORD , 'break')
-    if_terminal = Token(TokenType.KEYWORD , 'if')
-    end_if_terminal = Token(TokenType.KEYWORD , 'endif')
-    else_terminal = Token(TokenType.KEYWORD , 'else')
-    repeat_terminal = Token(TokenType.KEYWORD, 'repeat')
-    until_terminal = Token(TokenType.KEYWORD , 'until')
-    return_terminal = Token(TokenType.KEYWORD , 'return')
-    assign_terminal = Token(TokenType.SYMBOL , '=')
-    less_terminal = Token(TokenType.SYMBOL , '<')
-    equal_terminal = Token(TokenType.SYMBOL , '==')
-    plus_terminal = Token(TokenType.SYMBOL , '+')
-    minus_terminal = Token(TokenType.SYMBOL, '-')
-    mul_terminal = Token(TokenType.SYMBOL , '*')
-    
+
     def __init__(self, scanner):
         super().__init__(scanner, self.__grammar_setter)
 
@@ -54,10 +55,169 @@ class CMinusParser(ParserBase):
         pass
         # self.start_symbol_name = 'Program'
         # self.start_symbol_transition_diagram = program
+
+    transition_diagrams = {''}
+
     @staticmethod
     def __get_program():
-        program =
+        program = TransitionDiagram(None, First([dollar_terminal, int_terminal, void_terminal]), Follow([]), 'PROGRAM')
+        state2 = State(2, [], True)
+        state1 = State(1, [TerminalTransition(state2, dollar_terminal)])
+        state0 = State(0, [NonTerminalTransition(state1, CMinusParser.__get_declaration_list)])
+        program.init_state = state0
+        return program
 
+    @staticmethod
+    def __get_declaration_list():
+        declaration_list = TransitionDiagram(None, First([int_terminal, void_terminal], True),
+                                             Follow([dollar_terminal, ID_terminal,
+                                                     semi_column_terminal, NUM_terminal, pr_open_terminal,
+                                                     cr_open_terminal, cr_close_terminal, break_terminal, if_terminal,
+                                                     repeat_terminal, return_terminal]), 'DECLARATION-LIST')
+        state2 = State(2, [], True)
+        state1 = State(1, [NonTerminalTransition(state2, declaration_list)])
+        state0 = State(0,
+                       [EpsilonTransition(state2), NonTerminalTransition(state1, CMinusParser.__get_declaration_list)])
+        declaration_list.init_state = state0
+        return declaration_list
+
+    @staticmethod
+    def __get_declaration():
+        declaration = TransitionDiagram(None, First([int_terminal, void_terminal]),
+                                        Follow([dollar_terminal, ID_terminal,
+                                                semi_column_terminal, NUM_terminal, pr_open_terminal, int_terminal,
+                                                void_terminal,
+                                                cr_open_terminal, cr_close_terminal, break_terminal, if_terminal,
+                                                repeat_terminal, return_terminal]), 'DECLARATION')
+        state2 = State(2, [], True)
+        state1 = State(1, [NonTerminalTransition(state2, CMinusParser.__get_declaration_prime)])
+        state0 = State(0, [NonTerminalTransition(state1, CMinusParser.__get_declaration_initial)])
+        declaration.init_state = state0
+        return declaration
+
+    @staticmethod
+    def __get_declaration_initial():
+        declaration_initial = TransitionDiagram(None, First([int_terminal, void_terminal]), Follow(
+            [semi_column_terminal, bracket_open_terminal, pr_close_terminal, pr_open_terminal, comma_terminal]),
+                                                'DECLARATION-INITIAL')
+        state2 = State(2, [], True)
+        state1 = State(1, [TerminalTransition(state2, ID_terminal)])
+        state0 = State(0, [NonTerminalTransition(state1, CMinusParser.__get_type_specifier)])
+        declaration_initial.init_state = state0
+        return declaration_initial
+
+    @staticmethod
+    def __get_declaration_prime():
+        declaration_prime = TransitionDiagram(None,
+                                              First([semi_column_terminal, bracket_open_terminal, pr_open_terminal]),
+                                              Follow([dollar_terminal, ID_terminal,
+                                                      semi_column_terminal, NUM_terminal, pr_open_terminal,
+                                                      int_terminal,
+                                                      void_terminal,
+                                                      cr_open_terminal, cr_close_terminal, break_terminal, if_terminal,
+                                                      repeat_terminal, return_terminal]), 'DECLARATION-PRIME')
+
+        state1 = State(1, [], True)
+        state0 = State(0, [NonTerminalTransition(state1, CMinusParser.__get_fun_declaration_prime),
+                           NonTerminalTransition(state1, CMinusParser.__get_var_declaration_prime)])
+        declaration_prime.init_state = state0
+        return declaration_prime
+
+    @staticmethod
+    def __get_type_specifier():
+        type_specifier = TransitionDiagram(None, First([int_terminal, void_terminal]), Follow([ID_terminal]),
+                                           'Type-Specifier')
+        state1 = State(1, [], True)
+        state0 = State(0, [TerminalTransition(state1, int_terminal), TerminalTransition(state1, void_terminal)])
+        type_specifier.init_state = state0
+        return type_specifier
+
+    @staticmethod
+    def __get_var_declaration_prime():
+        var_declaration_prime = TransitionDiagram(None, First([semi_column_terminal, bracket_open_terminal]),
+                                                  Follow([dollar_terminal, ID_terminal,
+                                                          semi_column_terminal, NUM_terminal, pr_open_terminal,
+                                                          int_terminal,
+                                                          void_terminal,
+                                                          cr_open_terminal, cr_close_terminal, break_terminal,
+                                                          if_terminal,
+                                                          repeat_terminal, return_terminal]), 'VAR-DECLARATION-PRIME')
+        state4 = State(4, [], True)
+        state3 = State(3, [TerminalTransition(state4, semi_column_terminal)])
+        state2 = State(2, [TerminalTransition(state3, bracket_close_terminal)])
+        state1 = State(1, [TerminalTransition(state2, NUM_terminal)])
+        state0 = State(0, [TerminalTransition(state1, bracket_open_terminal),
+                           TerminalTransition(state4, semi_column_terminal)])
+        var_declaration_prime.init_state = state0
+        return var_declaration_prime
+
+    @staticmethod
+    def __get_fun_declaration_prime():
+        fun_declaration_prime = TransitionDiagram(None, First([pr_open_terminal]), Follow([dollar_terminal, ID_terminal,
+                                                                                           semi_column_terminal,
+                                                                                           NUM_terminal,
+                                                                                           pr_open_terminal,
+                                                                                           int_terminal,
+                                                                                           void_terminal,
+                                                                                           cr_open_terminal,
+                                                                                           cr_close_terminal,
+                                                                                           break_terminal,
+                                                                                           if_terminal,
+                                                                                           repeat_terminal,
+                                                                                           return_terminal]),
+                                                  'FUN-DECLARATION-PRIME')
+        state4 = State(4, [], True)
+        state3 = State(3, [NonTerminalTransition(state4, CMinusParser.__get_compound_stmt)])
+        state2 = State(2, [TerminalTransition(state3, pr_close_terminal)])
+        state1 = State(1, [NonTerminalTransition(state2, CMinusParser.__get_params)])
+        state0 = State(0, [TerminalTransition(state1, pr_open_terminal)])
+        fun_declaration_prime.init_state = state0
+        return fun_declaration_prime
+
+    @staticmethod
+    def __get_params():
+        params = TransitionDiagram(None, First([int_terminal, void_terminal]), Follow([pr_close_terminal]), 'PARAMS')
+        state4 = State(4, [], True)
+        state3 = State(3, [NonTerminalTransition(state4, CMinusParser.__get_params_list)])
+        state2 = State(2, [NonTerminalTransition(state3, CMinusParser.__get_param_prime)])
+        state1 = State(1, [TerminalTransition(state2, ID_terminal)])
+        state0 = State(0, [TerminalTransition(state1, int_terminal), TerminalTransition(state4, void_terminal)])
+        params.init_state = state0
+        return params
+
+    @staticmethod
+    def __get_params_list():
+        params_list = TransitionDiagram(None, First([comma_terminal], True), Follow([pr_close_terminal]), 'PARAM-LIST')
+        state3 = State(3, [], True)
+        state2 = State(2, [NonTerminalTransition(state3, params_list)])
+        state1 = State(1, [NonTerminalTransition(state2, CMinusParser.__get_param)])
+        state0 = State(0, [TerminalTransition(state1, comma_terminal), EpsilonTransition(state2)])
+        params_list.init_state = state0
+        return params_list
+
+    @staticmethod
+    def __get_param():
+        param = TransitionDiagram(None, First([int_terminal, void_terminal]),
+                                  Follow([pr_close_terminal, comma_terminal]), 'PARAM')
+        return param
+
+    @staticmethod
+    def __get_param_prime():
+        param_prime = TransitionDiagram(None, First([bracket_open_terminal], True),
+                                        Follow([comma_terminal, pr_close_terminal]), 'PARAM-PRIME')
+        state2 = State(2, [], True)
+        state1 = State(1, [TerminalTransition(state2, bracket_close_terminal)])
+        state0 = State(0, [TerminalTransition(state1, bracket_open_terminal), EpsilonTransition(state2)])
+        param_prime.init_state = state0
+        return param_prime
+
+    @staticmethod
+    def __get_compound_stmt():
+        compound_stmt = TransitionDiagram(None, First([cr_open_terminal]), Follow(
+            [dollar_terminal, ID_terminal, semi_column_terminal, NUM_terminal, pr_open_terminal, int_terminal,
+             void_terminal, cr_open_terminal, cr_close_terminal, break_terminal, if_terminal, end_if_terminal,
+             else_terminal, repeat_terminal, until_terminal, return_terminal], 'COMPOUND-STMT'))
+        return compound_stmt
 
 
 class SimpleArithmeticParser(ParserBase):
