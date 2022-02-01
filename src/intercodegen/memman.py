22c1725d21cb0f11b6cceb076fd15047e36037b3
Parsa Mohammadian <parsa2820@gmail.com>
2022-02-01 18:25:46 +0330

    Add doc

---
+++ src/intercodegen/memman.py
@@ -0,0 +1,29 @@
+class MemoryManager:
+    DATA_START_ADDRESS = 500
+    SIZE_BY_TYPE = {'int': 4, 'void': 0}
+
+    def __init__(self):
+        self.address = MemoryManager.DATA_START_ADDRESS
+        self.offset = 0
+        self.scope = 0
+        self.scope_size_stack = []
+
+    def get_address(self, type='int'):
+        address = self.address + self.offset
+        self.offset += MemoryManager.SIZE_BY_TYPE[type]
+        return address
+
+    def add_scope(self):
+        self.scope += 1
+        self.scope_size_stack.append(self.offset)
+        self.address += self.offset
+        self.offset = 0
+
+    def remove_scope(self):
+        self.offset = self.scope_size_stack.pop()
+        self.address -= self.offset
+        self.scope -= 1
+
+        
+
+
